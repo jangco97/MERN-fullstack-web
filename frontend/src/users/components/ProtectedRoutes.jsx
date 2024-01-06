839c0bf2b239d7c2f85a5fe11e3b55906772882a
jangco97 <qkrnslehd@naver.com>
2024-01-06 20:18:21 +0900

    [feat] 로그인 관련 페이지 추가

---
+++ frontend/src/users/components/ProtectedRoutes.jsx
@@ -0,0 +1,9 @@
+//로그인 안된 사람이 로그인 되어야 볼 수 있는 페이지 경로 들어갔을 경우 로그인페이지로 이동시킴
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { Navigate, Outlet } from 'react-router-dom';
+
+const ProtectedRoutes = ({ isLoggedIn }) => {
+  return isLoggedIn ? <Outlet /> : <Navigate to={'/auth'} />;
+};
+
+export default ProtectedRoutes;
