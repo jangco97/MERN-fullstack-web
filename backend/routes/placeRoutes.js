e53348a956c001bbf3a2e9d412a3e8f2586f0f87
jangco97 <qkrnslehd@naver.com>
2024-01-09 11:23:26 +0900

    [feat] placeRoutes파일 추가

---
+++ backend/routes/placeRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { check } = require("express-validator");
+const placeControllers = require("../controllers/placeControllers");
+
+const router = express.Router();
+
+router.get("/:pid", placeControllers.getPlaceById);
+
+router.get("/user/:uid", placeControllers.getPlacesByUserId);
+
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 7 }),
+    check("address").not().isEmpty(),
+  ],
+  placeControllers.createPlace
+);
+
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 7 })],
+  placeControllers.updatePlace
+);
+
+router.delete("/:pid", placeControllers.deletePlace);
+
+module.exports = router;
